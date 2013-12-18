1735b6e75234f371dc430f33361ea0d104dcaa33
sranso <sranso@gmail.com>
2013-12-18 12:15:19 -0500

    cells work, layout looks nice

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ Bundler.require
 Dir.glob('./lib/*.rb') do |model|
   require model
 end
-      # <div class="cell" style="<%=cell.state == "." ? @black : @blue %>"></div>
+
 module Name
   class App < Sinatra::Application
     @@game = Game.new
@@ -13,8 +13,8 @@ module Name
     get '/' do
       @@game.world.turn_world
       @local_game = @@game
-      @black = 'background-color: black;'
-      @blue = 'background-color: blue;'
+      @white = 'background-color: white;'
+      @crimson = 'background-color: crimson;'
       erb :index
     end
 
